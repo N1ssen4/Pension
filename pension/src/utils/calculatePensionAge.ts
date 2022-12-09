337078a7e073b7a5cb82be98fc57bb7226bde5d0
Mads Nissen <74189421+N1ssen4@users.noreply.github.com>
2022-12-09 12:30:10 +0100

    fixed pensionage calculations

--- pension/src/utils/calculatePensionAge.ts
+++ pension/src/utils/calculatePensionAge.ts
@@ -3,17 +3,17 @@ import { birthYear } from "./birthyear";
 //calculating the pension age.
 export const pensionAge = (age: number | null) => {
 
-    if (birthYear(age) <= 1954) {
+    if (birthYear(age) <= 1955) {
       return 66;
-    } else if (birthYear(age) <= 1955) {
+    } else if (birthYear(age) >= 1955 && birthYear(age) < 1963) {
       return 67;
-    } else if (birthYear(age) < 1962) {
+    } else if (birthYear(age) >= 1963 && birthYear(age) < 1967) {
       return 68;
-    } else if (birthYear(age) < 1966) {
+    } else if (birthYear(age) >= 1967 && birthYear(age) < 1971) {
       return 69;
-    } else if (birthYear(age) < 1970) {
+    } else if (birthYear(age) >= 1971 && birthYear(age) < 1975) {
       return 70;
-    } else if (birthYear(age) < 1974) {
+    } else if (birthYear(age) >= 1975 && birthYear(age) < 1979) {
       return 71;
     } else return 72;
 }

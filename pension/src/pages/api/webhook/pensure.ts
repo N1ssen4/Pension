b09e59ce01320cc3f77a5533f66425008a8bfa03
Mads Nissen <74189421+N1ssen4@users.noreply.github.com>
2022-12-17 13:00:50 +0100

    jaklsd

--- pension/src/pages/api/webhook/pensure.ts
+++ pension/src/pages/api/webhook/pensure.ts
@@ -59,7 +59,7 @@ export default async function Handler(
       await getPensionInfo(apiToken);
       await markExported(apiToken);
 
-      res.status(200).json((res : any) => res.Header)
+      res.status(200).send(res)
     } catch (err) {
       console.log(err);
 

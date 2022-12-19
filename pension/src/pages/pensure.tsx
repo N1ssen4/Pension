a4a9748a23bcc7130fcffc44e1cc0c19516a86e9
Mads Nissen <74189421+N1ssen4@users.noreply.github.com>
2022-12-19 10:47:44 +0100

    k

--- pension/src/pages/pensure.tsx
+++ pension/src/pages/pensure.tsx
@@ -3,11 +3,13 @@ import { useEffect } from "react";
 import { getPensionInfo } from "../services/pension.service";
 
 export const Pensure: NextPage = () => {
+  useEffect(() => {
+    const pensureID = localStorage.getItem("pensureID");
+    if (pensureID) {
+      getPensionInfo(pensureID);
+    }
+  },[]);
 
-const pensureID = localStorage.getItem("pensureID");
-if (pensureID) {
-  getPensionInfo(pensureID);
-}
   return (
     <>
       <div>Pensure Page</div>

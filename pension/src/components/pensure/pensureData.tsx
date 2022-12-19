17c33b75662ff1b4248a020b758bc3b871da5f8c
Mads Nissen <74189421+N1ssen4@users.noreply.github.com>
2022-12-19 13:03:21 +0100

    mo

--- pension/src/components/pensure/pensureData.tsx
+++ pension/src/components/pensure/pensureData.tsx
@@ -1,23 +1,19 @@
+import { divide } from "cypress/types/lodash";
 import { DocumentData } from "firebase/firestore";
 import Link from "next/link";
 import { useEffect, useState } from "react";
 import { getPensionInfo } from "../../services/pension.service";
 
 const PensureData = () => {
-  const [pensionInfo, setPensionInfo] = useState();
+  const [pensionInfo, setPensionInfo] = useState<DocumentData | undefined>();
 
   useEffect(() => {
     const ID = localStorage.getItem("pensureID");
     if (ID) {
       // Wait for the Promise to resolve before calling setPensionInfo
       (async () => {
-        const info = await getPensionInfo(ID);
-        // Add a check to see if the value returned by the getPensionInfo function is undefined
-        if (info) {
-          // Use the toObject method to convert the DocumentData type to a normal JavaScript object
-          const data = info.toObject();
-          setPensionInfo(data);
-        }
+        const data = await getPensionInfo(ID);
+        setPensionInfo(data);
       })();
     }
   }, []);
@@ -27,7 +23,19 @@ const PensureData = () => {
       <div className="space-y-6">
         <div className="w-[300px] space-y-3 rounded-t-2xl rounded-br-2xl border p-5 shadow">
           <div>Samlet pensionsindsamling:</div>
-          <div className="font-bold">{pensionInfo}</div>
+          <div className="font-bold">
+            {pensionInfo ? (
+              <>
+                <div>Indbetaling: {pensionInfo.Payment}</div>
+                <div>
+                  Pensionsforening: {pensionInfo.PensionProviderName}
+                </div>
+                <div>Pensionsopsparing: {pensionInfo.SavedValue}</div>
+              </>
+            ) : (
+              <div>Loading...</div>
+            )}
+          </div>
           <div className="flex justify-start"></div>
         </div>
         <div className="w-[300px] space-y-3 rounded-t-2xl rounded-br-2xl border p-5 shadow">

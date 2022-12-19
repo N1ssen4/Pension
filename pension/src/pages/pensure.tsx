b3426250401723f2fe2e5c07ce581a8c0effa552
Mads Nissen <74189421+N1ssen4@users.noreply.github.com>
2022-12-19 10:33:58 +0100

    firestoooore

--- pension/src/pages/pensure.tsx
+++ pension/src/pages/pensure.tsx
@@ -1,6 +1,15 @@
 import { NextPage } from "next";
+import { useEffect } from "react";
+import { getPensionInfo } from "../services/pension.service";
 
 export const Pensure: NextPage = () => {
+
+  useEffect(() => {
+    const pensureID = localStorage.getItem("pensureID")
+    if (pensureID) {
+      getPensionInfo(pensureID);
+    }
+  },[])
   return (
     <>
       <div>Pensure Page</div>
--- pension/src/services/pension.service.ts
+++ pension/src/services/pension.service.ts
@@ -1,4 +1,4 @@
-import { addDoc, collection, doc, updateDoc } from "firebase/firestore";
+import { addDoc, collection, doc, getDoc, setDoc, updateDoc } from "firebase/firestore";
 import { db } from "../utils/database/server";
 
 //Establish a connection to the database
@@ -6,5 +6,13 @@ const pensionCollectionRef = collection(db, "pension");
 
 //Add new pensionInfo to the pensioninfo collection
 export const createPensionInfo = async (id: string, pensionInfo: any[]) => {
-  await addDoc(pensionCollectionRef, {id, ...pensionInfo});
-};
+await setDoc(doc(db, "pension", id), {...pensionInfo})};
+
+export const getPensionInfo = async (id: string) => {
+    const docRef = doc(db, "pension", id)
+    const info = await getDoc(docRef)
+    if (info.exists()){
+        console.log(info.data)
+    }
+    console.log("No pension data found")
+}

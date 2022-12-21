d92c0c50a9d562fb8d7277366888814265566979
Mads <mads.rosenlund@hotmail.com>
2022-12-21 19:14:34 +0100

    removed unnessesary info button

--- pension/src/components/pension/PensionIntroduction.tsx
+++ pension/src/components/pension/PensionIntroduction.tsx
@@ -1,30 +1,24 @@
-import { QuestionMarkCircleIcon } from '@heroicons/react/24/outline';
-import React from 'react'
-import { InfoModal } from '../home/InfoModal';
+import { QuestionMarkCircleIcon } from "@heroicons/react/24/outline";
+import React from "react";
+import { InfoModal } from "../home/InfoModal";
 
 const PensionIntroduction = () => {
-  //state and function for handling modals. 
-  const [isModalOpen, setModalState] = React.useState(false);
-  const toggleModal = () => setModalState(!isModalOpen);
-  
   return (
     <>
       <div className="mx-12 space-y-5 ">
-        <div data-test-id="pensionPageTitle" className="mx-8 flex justify-center text-[20px] font-semibold">
+        <div
+          data-test-id="pensionPageTitle"
+          className="mx-8 flex justify-center text-[20px] font-semibold"
+        >
           Pension
-          <button onClick={toggleModal}>
-            <QuestionMarkCircleIcon data-test-id="QuestionMarkIcon" className="my-auto ml-2 h-[17px] w-[17px] " />
-          </button>
         </div>
-        <InfoModal isOpen={isModalOpen} onclose={toggleModal}>
-          Pensionsinfo
-        </InfoModal>
+
         <div data-test-id="pensionPageText" className="text-center">
           Her kan du se konsekvenserne af at ændre din pensionsalder:
         </div>
       </div>
     </>
   );
-}
+};
 
-export default PensionIntroduction
+export default PensionIntroduction;
